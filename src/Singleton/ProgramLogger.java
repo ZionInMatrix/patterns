eead2fec81b54b81f022f5a258a1e1e06ef0a27a
rudolfg <rudolf.gasparian@seznam.cz>
2022-08-03 09:35:45 +0200

    refactoring prototype

--- src/Singleton/ProgramLogger.java
+++ src/Singleton/ProgramLogger.java
@@ -4,7 +4,7 @@ public class ProgramLogger {
     private static ProgramLogger programLogger;
     private static String logFile = "This is log file. \n\n";
 
-    public static synchronized ProgramLogger getProgramLogger() {
+    public static ProgramLogger getProgramLogger() {
         if (programLogger == null) {
             programLogger = new ProgramLogger();
         }
